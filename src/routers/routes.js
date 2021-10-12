38df28753ea5d50debcfc1e12b44edda32ae81e0
ingridsj <ingridsjalmeida@gmail.com>
2021-10-12 20:15:02 -0300

    add: setting routes

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -1,20 +1,66 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { RoutesPrivate } from "./Private/Private";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { Redirect } from "react-router";
+
 import { HomePage } from "../pages/HomePage";
 import { SignIn } from "../pages/SignIn";
-import { DashBoard } from "../pages/DashBoard";
+import { Profile } from "../pages/Profile";
+import { HistoryPage } from "../pages/HistoryPage";
+import { Schedule } from "../pages/Schedule";
+import { NewSchedule } from "../pages/NewSchedule";
+
 import { StoreProvider } from "../hooks/useStore";
+import { Content } from "../pages/Content";
+import RouteWithSubRoutes from "../utils/RouteWithSubRoutes";
+import { ClientHelp } from "../pages/ClientHelp";
+
+export const routes = [
+  {
+    path: "/home",
+    component: HomePage,
+  },
+  {
+    path: "/login",
+    component: SignIn,
+  },
+  {
+    path: "/dashboard",
+    component: Content,
+    routes: [
+      {
+        path: "/dashboard/profile",
+        component: Profile,
+      },
+      {
+        path: "/dashboard/history",
+        component: HistoryPage,
+      },
+      {
+        path: "/dashboard/schedule",
+        component: Schedule,
+      },
+      {
+        path: "/dashboard/new-schedule",
+        component: NewSchedule,
+      },
+      {
+        path: "/dashboard/help",
+        component: ClientHelp,
+      },
+    ],
+  },
+];
 
 export function Routes() {
   return (
-    <BrowserRouter>
+    <Router>
       <StoreProvider>
         <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/login" component={SignIn} />
-          <RoutesPrivate path="/dashboard" component={DashBoard} />
+          <Redirect exact from="/" to="/home" />
+          {routes.map((route, i) => (
+            <RouteWithSubRoutes key={i} {...route} />
+          ))}
         </Switch>
       </StoreProvider>
-    </BrowserRouter>
+    </Router>
   );
 }
---
+++ src/utils/RouteWithSubRoutes.js
@@ -0,0 +1,12 @@
+import { Route } from "react-router-dom";
+
+const RouteWithSubRoutes = (route) => {
+  return (
+    <Route
+      path={route.path}
+      render={(props) => <route.component {...props} routes={route.routes} />}
+    />
+  );
+};
+
+export default RouteWithSubRoutes;
